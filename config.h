c67f7528904355006f944728844ec388f9c553e1
Thomas Topp <thomas@topp.cc>
2021-10-14 14:36:45 +0200

    Colored emoji support

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;             /* 1 means swallow floating windo
 static int showbar            = 1;             /* 0 means no bar */
 static int topbar             = 1;             /* 0 means bottom bar */
 static int viewontag          = 1;             /* Switch view on tag switch */
-static char *fonts[]          = { "monospace:size=10" };
+static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]     = "#222222";
 static char normbordercolor[] = "#444444";
