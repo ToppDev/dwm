de1b693f68da3e3bb34db1ec7997469d09f5e837
Thomas Topp <thomas@topp.cc>
2021-10-15 09:36:48 +0200

    Error/Warning fix

--- dwm.c
+++ dwm.c
@@ -1979,8 +1979,6 @@ sigterm(int unused)
 void
 spawn(const Arg *arg)
 {
-	if (arg->v == dmenucmd)
-		dmenumon[0] = '0' + selmon->num;
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
