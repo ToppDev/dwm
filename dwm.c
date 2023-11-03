0fa8bfa40c1e3c7bf1852eb83f619f6fd97acd5b
ToppDev <dev@topp.cc>
2023-11-03 10:22:40 +0100

    Autostart /home/topas/.config/dwm

--- dwm.c
+++ dwm.c
@@ -28,6 +28,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <X11/cursorfont.h>
@@ -294,7 +295,11 @@ static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
 
 /* variables */
+static const char autostartblocksh[] = "autostart_blocking.sh";
+static const char autostartsh[] = "autostart.sh";
 static const char broken[] = "broken";
+static const char dwmdir[] = "dwm";
+static const char configdir[] = ".config";
 static char stextc[STATUSLENGTH];
 static char stexts[STATUSLENGTH];
 static int screen;
@@ -1698,6 +1703,79 @@ void
 runautostart(void)
 {
     system("killall -q dwmblocks; dwmblocks &");
+
+    char *pathpfx;
+    char *path;
+    char *xdgconfighome;
+    char *home;
+    struct stat sb;
+
+    if ((home = getenv("HOME")) == NULL)
+        /* this is almost impossible */
+        return;
+
+    /* if $XDG_CONFIG_HOME is set and not empty, use $XDG_CONFIG_HOME/dwm,
+     * otherwise use ~/.config/dwm as autostart script directory
+     */
+    xdgconfighome = getenv("XDG_CONFIG_HOME");
+    if (xdgconfighome != NULL && *xdgconfighome != '\0') {
+        /* space for path segments, separators and nul */
+        pathpfx = ecalloc(1, strlen(xdgconfighome) + strlen(dwmdir) + 2);
+
+        if (sprintf(pathpfx, "%s/%s", xdgconfighome, dwmdir) <= 0) {
+            free(pathpfx);
+            return;
+        }
+    } else {
+        /* space for path segments, separators and nul */
+        pathpfx = ecalloc(1, strlen(home) + strlen(configdir)
+                             + strlen(dwmdir) + 3);
+
+        if (sprintf(pathpfx, "%s/%s/%s", home, configdir, dwmdir) < 0) {
+            free(pathpfx);
+            return;
+        }
+    }
+
+    /* check if the autostart script directory exists */
+    if (! (stat(pathpfx, &sb) == 0 && S_ISDIR(sb.st_mode))) {
+        /* the XDG conformant path does not exist or is no directory
+         * so we try ~/.dwm instead
+         */
+        char *pathpfx_new = realloc(pathpfx, strlen(home) + strlen(dwmdir) + 3);
+        if(pathpfx_new == NULL) {
+            free(pathpfx);
+            return;
+        }
+        pathpfx = pathpfx_new;
+
+        if (sprintf(pathpfx, "%s/.%s", home, dwmdir) <= 0) {
+            free(pathpfx);
+            return;
+        }
+    }
+
+    /* try the blocking script first */
+    path = ecalloc(1, strlen(pathpfx) + strlen(autostartblocksh) + 2);
+    if (sprintf(path, "%s/%s", pathpfx, autostartblocksh) <= 0) {
+        free(path);
+        free(pathpfx);
+    }
+
+    if (access(path, X_OK) == 0)
+        system(path);
+
+    /* now the non-blocking script */
+    if (sprintf(path, "%s/%s", pathpfx, autostartsh) <= 0) {
+        free(path);
+        free(pathpfx);
+    }
+
+    if (access(path, X_OK) == 0)
+        system(strcat(path, " &"));
+
+    free(pathpfx);
+    free(path);
 }
 
 void
